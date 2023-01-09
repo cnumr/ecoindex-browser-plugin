3a5c1c82a6d1f7cb3d35e1d54ba5080ed3be7a01
Vincent Vatelot <vincent.vatelot@ik.me>
2023-01-09 13:54:31 +0100

    ♻️ refact(background): Lint code and to to uniformize browser action

--- src/background/background.js
+++ src/background/background.js
@@ -1,34 +1,48 @@
 const apiUrl = 'https://bff.ecoindex.fr';
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    if (changeInfo.url) {
-        await getAndUpdateEcoindexData(changeInfo.url);
-    }
-});
+let currentBrowser;
 
-chrome.tabs.onActivated.addListener(async function (activeInfo) {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    if (tab.url) {
-        await getAndUpdateEcoindexData(tab.url);
-    }
-});
+if (navigator.userAgent.includes('Firefox')) {
+  currentBrowser = browser;
+} else {
+  currentBrowser = chrome;
+}
+// TODO: browserAction if Firefox else action...
 
 async function setBadgeUnknownGrade() {
-    await chrome.action.setBadgeBackgroundColor({color: '#5b5b5b'});
-    await chrome.action.setBadgeText({text: '?'});
-}
-
-async function getAndUpdateEcoindexData(url) {
-    fetch(`${apiUrl}?url=${url}`)
-        .then((r) => r.json())
-        .then(updateBadge);
+  await currentBrowser.browserAction.setBadgeBackgroundColor({ color: '#5b5b5b' });
+  await currentBrowser.browserAction.setBadgeText({ text: '?' });
 }
 
 async function updateBadge(ecoindexData) {
-    if (!ecoindexData['latest-result'].id) {
-        await setBadgeUnknownGrade();
-    } else {
-        const {color, grade: text} = ecoindexData['latest-result'];
-        await chrome.action.setBadgeBackgroundColor({color});
-        await chrome.action.setBadgeText({text});
-    }
+  if (!ecoindexData['latest-result'].id) {
+    await setBadgeUnknownGrade();
+  } else {
+    const { color, grade: text } = ecoindexData['latest-result'];
+    await currentBrowser.browserAction.setBadgeBackgroundColor({ color });
+    await currentBrowser.browserAction.setBadgeText({ text });
+  }
 }
+
+async function getAndUpdateEcoindexData(url) {
+  fetch(`${apiUrl}?url=${url}`)
+    .then((r) => r.json())
+    .then(updateBadge);
+}
+
+async function getBadgeInfo(url) {
+  await setBadgeUnknownGrade();
+
+  if (typeof url === 'string' && url.startsWith('http')) {
+    await getAndUpdateEcoindexData(url);
+  }
+}
+
+currentBrowser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (!changeInfo.url) return;
+  await getBadgeInfo(changeInfo.url);
+});
+
+currentBrowser.tabs.onActivated.addListener(async (activeInfo) => {
+  const [tab] = await currentBrowser.tabs.query({ active: true, currentWindow: true });
+  await getBadgeInfo(tab.url);
+});
